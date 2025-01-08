1dc71ccd26abe51a9ac68cffd74276283cc89914
saibharathn <nangunurisaibharath@gmail.com>
2025-01-08 00:32:30 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.css'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // This processes CSS files
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                type: 'asset/resource', // For handling images
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        hot: true,
+        port: 3000,
+    },
+};
